1517946d10a21c0e7c67f4a73c026c590c5a7303
root <root@vm1.asia-south1-a.c.myproj-214413.internal>
2019-06-11 10:09:38 +0000

    update1

--- usecase1.py
+++ usecase1.py
@@ -3,8 +3,17 @@ import matplotlib.pyplot as plt
 
 link = "https://datatank.stad.gent/4/mobiliteit/fietstellingencoupure.csv?limit=100000"
 df = pd.read_csv(link, sep=';')
-print(df.head())
-print(len(df))
-combined = df['datum'] + ' ' + df['tijd']
-print(combined.head)
-print(type(combined))
+#print(df.head())
+#print(len(df))
+#combined = df['datum'] + ' ' + df['tijd']
+df.index=pd.to_datetime(df['datum'] + ' ' + df['tijd'])
+df=df.drop(['datum','tijd'],axis=1)
+df=df.rename(columns={'ri Centrum': 'direction_center','ri Mariakerke':'direction_mariakerke'})
+#print(df.head)
+print(pd.Series(df.index).diff().value_counts())
+print(df.describe())
+df_both=df.sum(axis=1)
+df_quiet=df_both[df_both<5]
+print(df_quiet.head())
+print(df[(df['direction_center'] < 3) | (df['direction_mariakerke'] < 3)].head())
+print(df.resample('H').sum().mean())
